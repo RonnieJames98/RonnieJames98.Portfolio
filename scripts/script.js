b1a78ee0bd11a4957babdb05b423daaf8ef819c6
Ronnie James <ronniejespinosa1998@gmail.com>
2023-04-04 16:40:03 -0700

    Added Currency Converter
    Currency Converter Project added to Portfolio

--- scripts/script.js
+++ scripts/script.js
@@ -3,3 +3,28 @@ $("nav p").click(function(){
     $("nav ul").toggleClass("show");
     
 });
+
+
+function movingBackground(){
+    var headerWidth = document.getElementById("header").offsetWidth;
+    console.log(headerWidth);
+    var movementStrength = 25;
+    var height = movementStrength / $(window).height();
+    var width = movementStrength / $(window).width();
+    
+    if(headerWidth > 1100){
+        $("#header").mousemove(function(e){
+            var pageX = e.pageX - ($(window).width() / 2);
+            var pageY = e.pageY - ($(window).height() / 2);
+            var newvalueX = width * pageX * -1 - 25;
+            var newvalueY = height * pageY * -1 - 50;
+            $('#header').css("background-position", newvalueX+"px     "+newvalueY+"px");
+        });
+    } else {
+        console.log("Nope");
+        $("#header").off("mousemove");
+        $('#header').css("background-position", "center");
+    }
+}
+
+setInterval(movingBackground, 500);
